d1f1d8e3636e71fc6a01f8b2b5ac983524228746
Thomas Guesnon <>
2024-12-15 16:50:00 +0100

    color return all values

--- src/app/components/color-chart.tsx
+++ src/app/components/color-chart.tsx
@@ -9,7 +9,7 @@ export default function ColorChart({ data }: ColorChartProps) {
         <>
             <div className="flex flex-col gap-4 p-8">
                 {Object.values(data).map((value, index) => {
-                    return <ColorItem key={index} isBase={value.isBase} Hexa={value.hexa} Name={value.name} Rgb={value.Rgb} Hsl={value.Hsl} />
+                    return <ColorItem key={index} isBase={value.isBase} Hexa={value.hexa} Name={value.name} Rgb={value.rgb} Hsl={value.hsl} />
                 })}
             </div>
         </>
--- src/app/components/color-item.tsx
+++ src/app/components/color-item.tsx
@@ -12,7 +12,17 @@ export default function ColorItem({isBase, Hexa, Name, Rgb, Hsl}: ColorItemProps
             <div className={`w-20 h-8 rounded-md ${isBase ? `underline` : null}`} style={{ backgroundColor: Hexa }}></div>
             <div className="flex flex-row gap-2">
                 <p>{Hexa}</p>
-                <p>{Name}</p>
+                <p>- {Name}</p>
+                <p>- RGB:
+                {Object.values(Rgb).map((value, index) => {
+                    return (<span key={index} className="pr-2">{value},</span>)
+                })}
+                </p>
+                <p>- HSL:
+                {Object.values(Hsl).map((value, index) => {
+                    return (<span key={index} className="pr-2">{value},</span>)
+                })}
+                </p>
                 {isBase ? <p className="rounded-full px-3 py-1 w-fit h-fit text-xs text-blue-600 bg-blue-100">Base</p> : null}
             </div>
         </div>
